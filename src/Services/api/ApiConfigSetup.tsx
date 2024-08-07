fa756250c5d9db0388f9745a3750e69ec06ae017
arjunaNR-gf <arjun042896@gmail.com>
2024-08-07 23:41:26 +0530

    changed back to orgin base Url

--- src/Services/api/ApiConfigSetup.tsx
+++ src/Services/api/ApiConfigSetup.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const ClientApi =  axios.create({
-      baseURL: 'http://DESKTOP-IS87TE0:7062/',
+      baseURL: 'https://localhost:7062/',
       headers: {
         'Accept': 'application/json',
         //'Authorization': 'token <your-token-here>
