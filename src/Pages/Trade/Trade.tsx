b45bfa751b11f19ed8e6b62967445092059244e2
arjunaNR-gf <arjun042896@gmail.com>
2024-10-07 01:20:43 +0530

    added new fb address

--- src/Pages/Trade/Trade.tsx
+++ src/Pages/Trade/Trade.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Button from '../../Component/Button';
-import { getDailSpendMonthOrYear, getOverAllMonthDetails } from '../../Services/api/DailySpendApi';
+import { getDailSpendMonthOrYear, getOverAllMonthDetails, getPaymentMenu } from '../../Services/api/DailySpendApi';
 import { app } from '../../Services/FireBaseDB/Configuration';
 import './index.css'
 import { getDatabase, push, ref, set, get, remove } from 'firebase/database';
@@ -10,52 +10,82 @@ const PushToFirebaseFromSql = () => {
 
     const [data, setData] = useState([])
 
-    const FireBasePushFromSql = () => {
+    const firebaseConfig = (pathLoc: string) => {
+        return ref(getDatabase(app), pathLoc)
+    }
+
+    const Push_Sync = (pathLoc: string, data: any) => {
+       return set(push(firebaseConfig(pathLoc)), data).then((result) => { console.log(result) }).catch(err => { console.log('error!!..') })
+
+    }
+
+    const Get_sync = (pathLoc: string) => {
+        return get(firebaseConfig(pathLoc))
+    }
+
+    const Delete_Sync = (pathLoc: string, id: string) => {
+        remove(firebaseConfig(pathLoc + id))
+    }
+
+    const OverAllSpendPush_Sync = () => {
+        const overAllSpendAddress = 'DailySpend/Profile/OverAllSpendMonth'
+
         getOverAllMonthDetails().then(async (res) => {
             if (res.status == '200') {
-                console.log(res.data)
-                const db = getDatabase(app);
-                const dbRef = ref(db, 'DailySpend/Profile/OverAllSpendMonth')
-                const getData = await get(dbRef)
+
+                const getData = await Get_sync(overAllSpendAddress)
                 if (getData.exists()) {
                     const tempDB = getData.val()
                     const tempData = Object.keys(tempDB).map((key, i) => {
                         return key
                     })
-                    const dbf = getDatabase(app);
-                    const dbRef = ref(dbf, 'DailySpend/Profile/OverAllSpendMonth/' + tempData[0]);
-                    await remove(dbRef)
 
-                    const db = getDatabase(app)
-                    const pushRef = push(ref(db, 'DailySpend/Profile/OverAllSpendMonth'));
-                    set(pushRef, res.data).then((result) => {
-                        console.log('success')
-                    }).catch(err => {
-                        console.log(err, 'error')
-                    })
+                    await Delete_Sync(overAllSpendAddress+'/', tempData[0]);
+
+
+                    await Push_Sync(overAllSpendAddress, res.data);
+
                 }
                 else {
-                   const db = getDatabase(app)
-                    const pushRef = push(ref(db, 'DailySpend/Profile/OverAllSpendMonth'));
-                    set(pushRef, res.data).then((result) => {
-                        console.log('success')
-                    }).catch(err => {
-                        console.log(err, 'error')
-                    })
+
+                    await Push_Sync(overAllSpendAddress, res.data);
                 }
             }
         }).catch((err) => {
             console.log(" server error!!!")
         })
+    }
+
+    const PaymentMenu_sync = () => {
+        const addressPayment='DailySpend/Payment/DropDownList'
+        getPaymentMenu().then(async (res) => {
+            const getData = await Get_sync(addressPayment)
+            if (getData.exists()) {
+                const tempDB = getData.val()
+                const dataID = Object.keys(tempDB).map((key, i) => {
+                    return key
+                })[0]
+                console.log(dataID,'data id')
+                await Delete_Sync(addressPayment+'/',dataID)
+                await Push_Sync(addressPayment, res.data)
+            }else
+            {
+                Push_Sync(addressPayment, res.data)
+            }
+        })
+    }
 
 
-
+    const FireBasePushFromSql = () => {
+        //used to handle over all spend per month and year
+        OverAllSpendPush_Sync();
+        PaymentMenu_sync();
     }
 
     return (
         <>
-        <div className='sync-btn'></div>
-            <Button size='full' Btext='Sync'  onclick={() => { FireBasePushFromSql() }} />
+            <div className='sync-btn'></div>
+            <Button size='full' Btext='Sync' onclick={() => { FireBasePushFromSql() }} />
         </>
     )
 }
